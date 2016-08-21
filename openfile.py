01975ead1080e2d6b9422fe33aca7b656844dca6
Praveen Pourush <praveenpourush@gmail.com>
2016-08-22 02:30:16 +0530

    Update openfile.py

--- openfile.py
+++ openfile.py
@@ -3,7 +3,7 @@
 # open file
 
 with open("/tmp/foo.txt", "r") as f:
-  lines = f.readlines()
+  lines = f.readlines()   //readlines will read all the lines of the file and all the lines will be stored in "lines"
   print lines
   #print f
 
